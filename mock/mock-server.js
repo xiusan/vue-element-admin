ca2c2899913b7c8084944cb622dca40a9cb74fe2
xiaojinlu <xiaojinlu1990@Gmail.com>
2020-12-14 22:49:44 +0800

    添加模糊查询

--- mock/mock-server.js
+++ mock/mock-server.js
@@ -46,7 +46,7 @@ const responseFake = (url, type, respond) => {
 module.exports = app => {
   // parse app.body
   // https://expressjs.com/en/4x/api.html#req.body
-  // app.use(bodyParser.json())
+   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({
     extended: true
   }))
